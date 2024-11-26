b24d14a623b2ff3c1d650d535bf34ee89316678f
khaled <ujjalbhuiyan123@gmail.com>
2024-11-27 02:35:01 +0600

    implemented UrlMappingRepository logic

---
+++ app/Repositories/UrlMappingRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\UrlMapping;
+
+class UrlMappingRepository
+{
+    public function findByShortCode(string $shortCode): ?UrlMapping
+    {
+        return UrlMapping::where('short_code', $shortCode)->first();
+    }
+
+    public function findByOriginalUrl(string $originalUrl): ?UrlMapping
+    {
+        return UrlMapping::where('original_url', $originalUrl)->first();
+    }
+
+    public function create(array $data): UrlMapping
+    {
+        return UrlMapping::create($data);
+    }
+}
