44d62b362bb6ba688a20de502f938f5e7f27eff7
khaled <ujjalbhuiyan123@gmail.com>
2024-11-27 22:48:34 +0600

    implemented custom attribute logic

--- app/Models/UrlMapping.php
+++ app/Models/UrlMapping.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use function App\Helpers\api_prefix;
 
 class UrlMapping extends Model
 {
-    //
+    protected $fillable = [
+        'original_url',
+        'short_code'
+    ];
+
+    public function getShortUrlAttribute(): string
+    {
+        return url(api_prefix().'/' . $this->short_code);
+    }
 }
